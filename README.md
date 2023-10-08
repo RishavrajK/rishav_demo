87b7faad0ebda65ed4f1f1cd09bfc1bf7354b87c
RishavrajK <rishavraj3378kant@gmail.com>
2023-10-08 17:59:46 +0530

    added new file

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # rishav_demo
-This is my first git repository.
+This is my first git repository .
 <br>
   Author- Rishav raj 
---
+++ hello_world.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    cout<<"HELLO WORLD"<<endl;
+    cout<<"Hi this is a local changes";
+return 0;
+}
